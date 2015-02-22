b2c4ff57748da748f0b30be432e1fe68f8e9f975
PoseidonHoHaiChi <poseidonhohaichi@gmail.com>
2015-02-21 23:41:16 -0800

    Poseidon2

---
+++ HackInvest/FSLineChart/UIColor+FSPalette.h
@@ -0,0 +1,17 @@
+#import <UIKit/UIKit.h>
+
+@interface UIColor (FSPalette)
+
++ (instancetype)fsRed;
++ (instancetype)fsOrange;
++ (instancetype)fsYellow;
++ (instancetype)fsGreen;
++ (instancetype)fsLightBlue;
++ (instancetype)fsDarkBlue;
++ (instancetype)fsPurple;
++ (instancetype)fsPink;
++ (instancetype)fsDarkGray;
++ (instancetype)fsLightGray;
+
+
+@end 
